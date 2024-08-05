863354ccb813a080d98c2d7dfc0ebad2f5e7e271
Andy Loughran <104910888+andyloughran@users.noreply.github.com>
2024-08-05 17:16:57 +0100

    Update src/lib/strings.js
    Co-authored-by: Nick Heal <152856676+nickhealGDS@users.noreply.github.com>

--- src/lib/strings.js
+++ src/lib/strings.js
@@ -2,7 +2,14 @@ const { randomBytes } = require("crypto");
 
 module.exports = {
   generateNonce: function generateNonce() {
-    const buffer = await new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, function(ex, buffer) {
+        if (ex) {
+          reject("error generating token");
+        }
+        resolve(buffer.toString("hex"));
+      });
+    })
       crypto.randomBytes(16, function(ex, buffer) {
         if (ex) {
           reject("error generating token");
