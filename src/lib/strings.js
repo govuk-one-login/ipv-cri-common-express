63848a16714c962ba9a9612f5dabfb73557bb629
Andrew Loughran <andrew.loughran@digital.cabinet-office.gov.uk>
2024-08-05 17:10:51 +0100

    Switched crypto to non-blocking mode

--- src/lib/strings.js
+++ src/lib/strings.js
@@ -2,6 +2,12 @@ const { randomBytes } = require("crypto");
 
 module.exports = {
   generateNonce: function generateNonce() {
-    return randomBytes(16).toString("hex");
-  },
+    const buffer = await new Promise((resolve, reject) => {
+      crypto.randomBytes(16, function(ex, buffer) {
+        if (ex) {
+          reject("error generating token");
+        }
+        resolve(buffer.toString("hex"));
+      });
+    })
 };
