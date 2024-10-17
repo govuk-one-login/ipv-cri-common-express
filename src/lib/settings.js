c3b8536030951e7372a1ed23f19905479dff0a9c
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2024-10-17 16:53:19 +0100

    OJ-2838: Add defaults for env vars as per README

--- src/lib/settings.js
+++ src/lib/settings.js
@@ -19,12 +19,18 @@ module.exports = {
     app.set("APP.GTM.UA_CONTAINER_ID", uaContainerId);
     app.set("APP.GTM.GA4_ENABLED", ga4Enabled);
     app.set("APP.GTM.UA_ENABLED", uaEnabled);
-    app.set("APP.GTM.GA4_PAGE_VIEW_ENABLED", ga4PageViewEnabled);
-    app.set("APP.GTM.GA4_FORM_RESPONSE_ENABLED", ga4FormResponseEnabled);
-    app.set("APP.GTM.GA4_FORM_ERROR_ENABLED", ga4FormErrorEnabled);
-    app.set("APP.GTM.GA4_FORM_CHANGE_ENABLED", ga4FormChangeEnabled);
-    app.set("APP.GTM.GA4_NAVIGATION_ENABLED", ga4NavigationEnabled);
-    app.set("APP.GTM.GA4_SELECT_CONTENT_ENABLED", ga4SelectContentEnabled);
+    app.set("APP.GTM.GA4_PAGE_VIEW_ENABLED", ga4PageViewEnabled ?? true);
+    app.set(
+      "APP.GTM.GA4_FORM_RESPONSE_ENABLED",
+      ga4FormResponseEnabled ?? true,
+    );
+    app.set("APP.GTM.GA4_FORM_ERROR_ENABLED", ga4FormErrorEnabled ?? true);
+    app.set("APP.GTM.GA4_FORM_CHANGE_ENABLED", ga4FormChangeEnabled ?? true);
+    app.set("APP.GTM.GA4_NAVIGATION_ENABLED", ga4NavigationEnabled ?? true);
+    app.set(
+      "APP.GTM.GA4_SELECT_CONTENT_ENABLED",
+      ga4SelectContentEnabled ?? true,
+    );
     app.set("APP.GTM.ANALYTICS_DATA_SENSITIVE", analyticsDataSensitive ?? true);
   },
 
