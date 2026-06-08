f2816ae34cd6c520683c36695b7fba457caa5fa0
Suraj Kumar <sk96.uk@gmail.com>
2026-06-08 15:48:05 +0100

    fix: don't overwrite an existing res.locals.translate (breaks hmpo-components label interpolation)

--- src/lib/i18next/replace-translate.js
+++ src/lib/i18next/replace-translate.js
@@ -1,7 +1,9 @@
 const replaceTranslate = (req, res, next) => {
   const t = req.i18n.getFixedT(req.i18n.language);
   req.translate = t;
-  res.locals.translate = (key, opts) => t(key, { ...res.locals, ...opts });
+  if (typeof res.locals.translate !== "function") {
+    res.locals.translate = (key, opts) => t(key, { ...res.locals, ...opts });
+  }
   next();
 };
 
